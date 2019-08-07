395041d412d530cb85157ec0b22e6abf4809df6a
Stephen Gacio <sgacio@mail.usf.edu>
2019-08-07 13:31:37 -0400

    test array written, base tests started

--- public/main.js
+++ public/main.js
@@ -1,7 +1,14 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+const newArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+const shuffleArray = () => {
+  for (let i = newArray.length - 1; i > 0; i--) {
+    // console.log(i)
+    const random = Math.floor(Math.random() * newArray.length)
+    console.log(random)
   }
+
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document
+  .querySelector('.button')
+  .addEventListener('click', shuffleArray)
